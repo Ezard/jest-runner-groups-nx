00586c8b8b8c041d2097cacfc59d5a10fc2ee852
Ben Ezard <ezard.ben@gmail.com>
2022-04-24 18:14:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const GroupRunner = require('jest-runner-groups');
+
+class NxGroupRunner extends GroupRunner {
+  runTests(tests, watcher, onStart, onResult, onFailure, options) {
+    const config = JSON.parse(process.argv[2]);
+    if (config?.overrides?.group) {
+      process.argv.push(`--group=${config.overrides.group}`);
+    }
+    return super.runTests(tests, watcher, onStart, onResult, onFailure, options);
+  }
+}
+
+module.exports = NxGroupRunner;
