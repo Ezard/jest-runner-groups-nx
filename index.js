961a7ab48651bdf6817c0222095763711855b1f8
Ben Ezard <ezard.ben@gmail.com>
2022-04-24 23:47:00 +0100

    fix: fix error caused by running tests outside of Nx (#1)
    * fix: fix error caused by running tests outside of Nx
    
    * 1.0.2

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const GroupRunner = require('jest-runner-groups');
 
 class NxGroupRunner extends GroupRunner {
   runTests(tests, watcher, onStart, onResult, onFailure, options) {
-    const config = JSON.parse(process.argv[2]);
-    if (config?.overrides?.group) {
-      process.argv.push(`--group=${config.overrides.group}`);
+    if (process.argv.length === 2) {
+      const config = JSON.parse(process.argv[2]);
+      if (config?.overrides?.group) {
+        process.argv.push(`--group=${config.overrides.group}`);
+      }
     }
     return super.runTests(tests, watcher, onStart, onResult, onFailure, options);
   }
