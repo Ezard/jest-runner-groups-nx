4afe776010eacb980d9bd2d085b17e5f6a5432f4
Ben Ezard <ezard.ben@gmail.com>
2022-04-25 09:43:46 +0100

    fix: fix num args check using 2 instead of 3 (#2)
    * fix: fix num args check using 2 instead of 3
    
    * Bump version to 1.0.3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const GroupRunner = require('jest-runner-groups');
 
 class NxGroupRunner extends GroupRunner {
   runTests(tests, watcher, onStart, onResult, onFailure, options) {
-    if (process.argv.length === 2) {
+    if (process.argv.length === 3) {
       const config = JSON.parse(process.argv[2]);
       if (config?.overrides?.group) {
         process.argv.push(`--group=${config.overrides.group}`);
