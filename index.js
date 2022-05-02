ae0addba89b665982d76d34dc22e99b1d3036f56
Ben Ezard <ezard.ben@gmail.com>
2022-05-02 22:14:49 +0100

    feat: improve error handling for non-Nx situations (#6)

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const GroupRunner = require('jest-runner-groups');
 
 class NxGroupRunner extends GroupRunner {
   runTests(tests, watcher, onStart, onResult, onFailure, options) {
-    if (process.argv.length === 3) {
+    try {
       const config = JSON.parse(process.argv[2]);
       if (config?.overrides?.group) {
         process.argv.push(`--group=${config.overrides.group}`);
       }
+    } catch (e) {
+      console.error(e);
     }
     return super.runTests(tests, watcher, onStart, onResult, onFailure, options);
   }
